a956af5fe991ae162d6dd9a3fb70efc0a2ed0a90
supriyadc2006-commits <supriyadc2006@gmail.com>
2026-04-17 10:44:40 +0530

    Update RAG_App.py

--- RAG_App.py
+++ RAG_App.py
@@ -24,7 +24,7 @@ groq_api_key = st.secrets["GROQ_API_KEY"]
 
 # ------------------ UI ------------------
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
-st.image("")
+st.image("ADA certificate.pdf")
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 
 # ------------------ SESSION STATE ------------------
